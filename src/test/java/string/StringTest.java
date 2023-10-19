ed3f461af9c26ee512d7123469c983ac61688b09
Mehrad <bahiraei.h@gmail.com>
2023-10-19 20:53:23 +0500

    #6 finished.

--- src/test/java/string/StringTest.java
+++ src/test/java/string/StringTest.java
@@ -24,4 +24,18 @@ public class StringTest {
         time = workInString.calculateTimeForFindSubString(str1, str2);
         Assertions.assertNotNull(time);
     }
+
+    @Test
+    void giveAnArrayFromADocument(){
+//        given
+        String path= "E:\\algorithms_practice\\src\\main\\resources\\input.text";
+        int length=0;
+
+        //        then
+        WorkInString workInString = new WorkInString();
+        char[] finalCharArray =workInString.convertTextFileToCharArray(path);
+        length = finalCharArray.length;
+        Assertions.assertNotEquals(length, 0);
+
+    }
 }
