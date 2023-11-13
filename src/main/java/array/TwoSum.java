5ae6d5af8946a79ec9c564c51b8557be63cf62bc
Mehrad <bahiraei.h@gmail.com>
2023-11-13 18:49:59 +0500

    closes #10.

--- src/main/java/array/TwoSum.java
+++ src/main/java/array/TwoSum.java
@@ -1,6 +1,5 @@
 package array;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,33 +13,35 @@ public class TwoSum {
 
 
     Map minesMap = new HashMap<Integer, Integer>();
-    Integer[] resultArr = new Integer[2];
+    int[] resultArr = new int[2];
 
     // helper func: fill HashMap put mines
-    void fillingMinesMap(Integer[] arr, Integer target) {
+    void fillingMinesMap(int[] arr, Integer target) {
         for (int i = 0; i < arr.length; i++) { //O(n)
             minesMap.put(target - arr[i], i);
         }
     }
 
-    public Integer[] findIndices(Integer[] arr, Integer target) {
+    public int[] findIndices(int[] arr, Integer target) {
 
 // check for invalid inputs
 
-        System.out.println("arr = " + Arrays.deepToString(arr));
+//        System.out.println("arr = " + Arrays.deepToString(arr));
 // in main func: ask HM in loop for arr
         for (int i = 0; i < arr.length; i++) { //O(n)
             this.fillingMinesMap(arr, target);
             if (minesMap.containsKey(arr[i])) { //O(1)
-                // solved
-                resultArr[0] = i;
-                System.out.println("index 1 = " + i);
-                System.out.println("V1 = " + arr[i]);
                 Integer secondIndex = (Integer) minesMap.get(arr[i]);
-                resultArr[1] = secondIndex;
-                System.out.println("index 2 = " + secondIndex);
-                System.out.println("V2 = " + arr[secondIndex]);
-                return resultArr;
+                if (!(i == secondIndex)) {
+                    // solved
+                    resultArr[0] = i;
+                    System.out.println("index 1 = " + i);
+                    System.out.println("V1 = " + arr[i]);
+                    resultArr[1] = secondIndex;
+                    System.out.println("index 2 = " + secondIndex);
+                    System.out.println("V2 = " + arr[secondIndex]);
+                    return resultArr;
+                }
             }
         }
 
