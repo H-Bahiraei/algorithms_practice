8004fb5185b1ff4d4060233a83b4b55e3e9674e0
Mehrad <bahiraei.h@gmail.com>
2024-02-19 11:57:51 +0500

    to be continued #52

--- src/main/java/recursive/EmployyLife.java
+++ src/main/java/recursive/EmployyLife.java
@@ -1,7 +1,6 @@
 package recursive;
 
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 /**
@@ -12,45 +11,7 @@ import java.util.Scanner;
  */
 public class EmployyLife {
 
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        int[] deadlines = new int[n];
-        for (int i = 0; i < n; i++) {
-            deadlines[i] = scanner.nextInt();
-        }
-
-        // Use a priority queue to sort tasks by their deadlines (ascending order)
-        PriorityQueue<Task> tasks = new PriorityQueue<>((t1, t2) -> Integer.compare(t1.deadline, t2.deadline));
-
-        // Add tasks to the queue with their corresponding deadlines
-        for (int i = 0; i < n; i++) {
-            tasks.add(new Task(i + 1, deadlines[i]));
-        }
-
-        int completedTasks = 0;
-        int currentTime = 0;
-
-        // Process tasks as long as we haven't exceeded the deadline or completed all tasks
-        while (currentTime <= deadlines[deadlines.length - 1] && !tasks.isEmpty()) {
-            Task currentTask = tasks.poll();
-
-            // Check if we can still complete the current task before its deadline
-            if (currentTime <= currentTask.deadline) {
-                completedTasks++;
-                currentTime++;
-            } else {
-                // Break as exceeding the deadline for this task means we can't complete any more
-                break;
-            }
-        }
-
-        System.out.println(completedTasks);
-    }
-
-    public static void main1(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] inputArray = new int[n];
@@ -91,16 +52,6 @@ public class EmployyLife {
         }
 
         return count;
-    }
-
-    private static class Task {
-        int id;
-        int deadline;
-
-        public Task(int id, int deadline) {
-            this.id = id;
-            this.deadline = deadline;
-        }
     }
 
 }
